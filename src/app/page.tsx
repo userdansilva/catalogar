af169670debcae9b34ecd2bbd641e1df58e1853f
Daniel Sousa <daniel.sousa@elo.com.br>
2024-03-06 20:42:52 -0300

    reset repository

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1>
-        Welcome to Catalogar!
-      </h1>
-    </div>
-  );
-}
