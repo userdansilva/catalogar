b9de172d11203899f83a98f78ee6fc0d9c9250ec
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-05-31 22:02:18 -0300

    chore: ajustes no loading

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import logo from "@/assets/images/logo.svg";
+import { LoaderCircle } from "lucide-react";
+
 export default function Loading() {
   return (
-    <div>
-      Loading...
+    <div className="flex h-[calc(100vh-(64px+32px))] w-full flex-col items-center justify-center gap-4">
+      <Image src={logo} height={40} alt="Logo" />
+      <div className="flex items-center gap-2">
+        <LoaderCircle className="size-4 animate-spin" />
+        <span className="text-lg font-medium">Carregando...</span>
+      </div>
     </div>
   );
 }
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import logo from "@/assets/images/logo.svg";
+import { LoaderCircle } from "lucide-react";
+
 export default function Loading() {
   return (
-    <div>
-      Loading...
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <Image src={logo} height={40} alt="Logo" />
+      <div className="flex items-center gap-2">
+        <LoaderCircle className="animate-spin" />
+        <span className="text-xl font-medium">Carregando...</span>
+      </div>
     </div>
   );
 }
