be631a20b05c2cf1b1044de0b5275d96175ed802
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-06-27 17:38:56 -0300

    add botão de voltar no preview

--- src/app/(preview)/preview/layout.tsx
+++ src/app/(preview)/preview/layout.tsx
@@ -1,6 +1,9 @@
 import { CatalogLayout } from "@/components/catalog-layout";
 import { routes } from "@/routes";
 import { getUser } from "@/services/get-user";
+import { Button } from "@/shadcn/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import { PropsWithChildren, ReactNode } from "react";
 
 export default async function PreviewLayout({
@@ -16,13 +19,24 @@ export default async function PreviewLayout({
   if (!theme || !company) return null;
 
   return (
-    <CatalogLayout
-      baseUrl={routes.preview.url}
-      company={company}
-      theme={theme}
-    >
-      {children}
-      {drawerdialog}
-    </CatalogLayout>
+    <div>
+      <div className="container bg-primary">
+        <Button variant="link" className="dark pl-0" size="sm" asChild>
+          <Link href={routes.dashboard.url}>
+            <ChevronLeft />
+            Voltar para Página Inicial
+          </Link>
+        </Button>
+      </div>
+
+      <CatalogLayout
+        baseUrl={routes.preview.url}
+        company={company}
+        theme={theme}
+      >
+        {children}
+        {drawerdialog}
+      </CatalogLayout>
+    </div>
   );
 }
