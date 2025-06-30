dd9636ed4714d9bca53e99fd57826e680855d563
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-06-30 16:59:34 -0300

    chore: melhoria de responsividade

--- src/app/(sidebar)/layout.tsx
+++ src/app/(sidebar)/layout.tsx
@@ -24,7 +24,7 @@ export default async function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
 
-      <SidebarInset>
+      <SidebarInset className="mx-auto max-w-6xl">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger />
