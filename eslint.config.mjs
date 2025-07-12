e3a5e533dc9e167015b89e6527998d7a49f03dd6
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-07-12 19:54:49 -0300

    chore: instalação do prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     ignorePatterns: ["src/shadcn/*"],
     rules: {
       "import/no-unresolved": "error",
