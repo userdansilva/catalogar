4afd8939d10f6cfc1a7cb761e5e7b30179d3cc2e
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-07-15 22:48:49 -0300

    chore: remoção de texto de meus catálogos

--- src/app/(sidebar)/meus-catalogos/layout.tsx
+++ src/app/(sidebar)/meus-catalogos/layout.tsx
@@ -1,18 +1,11 @@
 import { PropsWithChildren } from "react";
 import { routes } from "@/routes";
-import { PageHeader } from "@/components/layout/page-header";
 import { PrevButton } from "@/components/prev-button";
 
 export default function MyCatalogsLayout({ children }: PropsWithChildren) {
   return (
     <div className="space-y-6">
       <PrevButton fallbackUrl={routes.dashboard.url} />
-
-      <PageHeader
-        title={routes.catalog.title}
-        description={routes.catalog.description}
-      />
-
       {children}
     </div>
   );
