cc8016a7c9a75b8b30bc4f5d20c000425375e676
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-08-05 19:20:51 -0300

    chore: ajustes gerais e correção de css

--- apps/catalogar/eslint.config.mjs
+++ apps/catalogar/eslint.config.mjs
@@ -1,11 +1,4 @@
 import { nextJsConfig } from "@catalogar/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
-const config = [
-  ...nextJsConfig,
-  {
-    ignores: ["src/shadcn/*"],
-  },
-];
-
-export default config;
+export default nextJsConfig;
--- apps/catalogar/src/app/layout.tsx
+++ apps/catalogar/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@catalogar/ui/globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ReactNode } from "react";
 import { Toaster } from "@catalogar/ui/components/sonner";
