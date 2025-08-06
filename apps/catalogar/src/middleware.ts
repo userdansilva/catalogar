dfe15bec0e1b9678aefd42a5a5b8f922bf9cb26d
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-08-06 19:48:20 -0300

    chore: ajuste no middleware (token refresh)

--- apps/catalogar/src/middleware.ts
+++ apps/catalogar/src/middleware.ts
@@ -1,8 +1,27 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+  const { pathname } = request.nextUrl;
+
+  const authRes = await auth0.middleware(request);
+  const session = await auth0.getSession(request);
+
+  if (pathname.startsWith("/auth")) {
+    return authRes;
+  }
+
+  if (session) {
+    await auth0.getAccessToken(request, authRes);
+  }
+
+  if (!session) {
+    return NextResponse.redirect(
+      new URL("/auth/login", request.nextUrl.origin)
+    );
+  }
+
+  return authRes;
 }
 
 export const config = {
@@ -14,6 +33,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - entrar
+     * - @
      */
     "/((?!api|_next/static|_next/image|favicon.ico|entrar|@).*)",
   ],
