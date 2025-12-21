b4b12bf99ec17221d7e8b9c87e73a6075b6ccf48
Daniel Silva <daniel.sousasilva97@outlook.com>
2025-12-21 13:51:10 -0300

    chore: instalação do biome e remoção do eslint e prettier

--- apps/catalogar/src/app/(public)/entrar/page.tsx
+++ apps/catalogar/src/app/(public)/entrar/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
 "use client";
 
 import Image from "next/image";
--- packages/eslint-config/prettier.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export const prettierConfig = {
-  semi: true,
-  singleQuote: false,
-  tabWidth: 2,
-  trailingComma: "all",
-  plugins: [import("prettier-plugin-tailwindcss")],
-  tailwindFunctions: ["clsx"],
-};
