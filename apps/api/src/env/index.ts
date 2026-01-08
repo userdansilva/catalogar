6153ad28421bf5f22631013412aa35c975d55080
Daniel Silva <daniel.sousasilva97@outlook.com>
2026-01-08 09:04:23 -0300

    estrutura base finalizada

---
+++ apps/api/src/env/index.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  PORT: z.coerce.number().default(3333),
+});
+
+export const env = envSchema.parse(process.env);
