ddd0584e50f725947a5e215a3633a367b9cdf37a
Daniel Silva <daniel.sousasilva97@outlook.com>
2026-05-04 21:46:14 -0300

    chore: migrate database, add generated files

--- apps/catalogar/prisma/migrations/20260505003943_initial_migration/migration.sql
+++ apps/catalogar/prisma/migrations/20260505003943_initial_migration/migration.sql
@@ -1,6 +1,3 @@
--- CreateEnum
-CREATE TYPE "FileType" AS ENUM ('WEBP', 'JPG', 'PNG', 'SVG');
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
@@ -56,6 +53,22 @@ CREATE TABLE "themes" (
 );
 
 -- CreateTable
+CREATE TABLE "theme_logos" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "size" BIGINT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "altText" TEXT NOT NULL,
+    "themeId" TEXT NOT NULL,
+    "catalogId" TEXT NOT NULL,
+
+    CONSTRAINT "theme_logos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "product_types" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -100,47 +113,15 @@ CREATE TABLE "catalog_items" (
 );
 
 -- CreateTable
-CREATE TABLE "files" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "size" BIGINT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "type" TEXT NOT NULL,
-    "catalogId" TEXT NOT NULL,
-    "width" INTEGER,
-    "height" INTEGER,
-    "altText" TEXT,
-    "position" INTEGER,
-
-    CONSTRAINT "files_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "images" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "size" BIGINT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "width" INTEGER,
-    "height" INTEGER,
-    "altText" TEXT,
-    "catalogId" TEXT NOT NULL,
-
-    CONSTRAINT "images_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "catalog_item_images" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "size" BIGINT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "width" INTEGER,
-    "height" INTEGER,
-    "altText" TEXT,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "altText" TEXT NOT NULL,
     "position" INTEGER NOT NULL,
     "catalogItemId" TEXT NOT NULL,
     "catalogId" TEXT NOT NULL,
@@ -175,10 +156,10 @@ CREATE UNIQUE INDEX "themes_catalogId_key" ON "themes"("catalogId");
 CREATE UNIQUE INDEX "themes_logoId_key" ON "themes"("logoId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "files_url_key" ON "files"("url");
+CREATE UNIQUE INDEX "theme_logos_url_key" ON "theme_logos"("url");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "images_url_key" ON "images"("url");
+CREATE UNIQUE INDEX "theme_logos_themeId_key" ON "theme_logos"("themeId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "catalog_item_images_url_key" ON "catalog_item_images"("url");
@@ -199,7 +180,10 @@ ALTER TABLE "companies" ADD CONSTRAINT "companies_catalogId_fkey" FOREIGN KEY ("
 ALTER TABLE "themes" ADD CONSTRAINT "themes_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "catalogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "themes" ADD CONSTRAINT "themes_logoId_fkey" FOREIGN KEY ("logoId") REFERENCES "images"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "theme_logos" ADD CONSTRAINT "theme_logos_themeId_fkey" FOREIGN KEY ("themeId") REFERENCES "themes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "theme_logos" ADD CONSTRAINT "theme_logos_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "catalogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "product_types" ADD CONSTRAINT "product_types_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "catalogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -212,12 +196,6 @@ ALTER TABLE "catalog_items" ADD CONSTRAINT "catalog_items_catalogId_fkey" FOREIG
 
 -- AddForeignKey
 ALTER TABLE "catalog_items" ADD CONSTRAINT "catalog_items_productTypeId_fkey" FOREIGN KEY ("productTypeId") REFERENCES "product_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "files" ADD CONSTRAINT "files_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "catalogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "images" ADD CONSTRAINT "images_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "catalogs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "catalog_item_images" ADD CONSTRAINT "catalog_item_images_catalogItemId_fkey" FOREIGN KEY ("catalogItemId") REFERENCES "catalog_items"("id") ON DELETE CASCADE ON UPDATE CASCADE;
